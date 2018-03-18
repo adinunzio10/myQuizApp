3727227a10f7b66d2bb58d7492001eadf3f69630
adinunzio10 <adinuinzio10@gmail.com>
2018-03-17 21:46:54 -0400

    Layout Draft 1

--- app/src/main/java/com/example/android/myquizapp/MainActivity.java
+++ app/src/main/java/com/example/android/myquizapp/MainActivity.java
@@ -1,7 +1,11 @@
 package com.example.android.myquizapp;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.CheckBox;
+import android.widget.RadioButton;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,4 +14,43 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    boolean questionOneCorrect;
+    boolean questionTwoCorrect;
+
+    public void gradeQuestionOne() {
+        // Figure out if question 1 is correct
+        CheckBox answerOneQuestionOneCheckBox = findViewById(R.id.answerOneQuestionOne);
+        boolean answerOneQuestionOne = answerOneQuestionOneCheckBox.isChecked();
+        CheckBox answerTwoQuestionOneCheckBox = findViewById(R.id.answerTwoQuestionOne);
+        boolean answerTwoQuestionOne = answerTwoQuestionOneCheckBox.isChecked();
+        CheckBox answerThreeQuestionOneCheckBox = findViewById(R.id.answerThreeQuestionOne);
+        boolean answerThreeQuestionOne = answerThreeQuestionOneCheckBox.isChecked();
+        CheckBox answerFourQuestionOneCheckBox = findViewById(R.id.answerFourQuestionOne);
+        boolean answerFourQuestionOne = answerFourQuestionOneCheckBox.isChecked();
+        if (answerOneQuestionOne && answerTwoQuestionOne && answerFourQuestionOne && !answerThreeQuestionOne) {
+            questionOneCorrect = true;
+        }
+    }
+
+    public void gradeQuestionTwo() {
+        // Figure out if question 2 is correct
+        RadioButton answerOneQuestionTwoRadioButton = findViewById(R.id.answerOneQuestionTwo);
+        boolean answerOneQuestionTwo = answerOneQuestionTwoRadioButton.isChecked();
+        if (answerOneQuestionTwo) {
+            questionTwoCorrect = true;
+        }
+    }
+
+    public void gradeTest(View view) {
+        gradeQuestionOne();
+        gradeQuestionTwo();
+        if (questionOneCorrect && questionTwoCorrect) {
+            Toast.makeText(this, "Correct!", Toast.LENGTH_SHORT).show();
+        } else {
+            Toast.makeText(this, "Incorrect!", Toast.LENGTH_SHORT).show();
+        }
+    }
+
 }
+
